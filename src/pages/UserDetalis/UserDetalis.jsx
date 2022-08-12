a94fc7d037fa74da43b630f7810030a448920e76
Erhan <erhankubanych@gmail.com>
2022-08-12 15:38:14 +0600

    feat: did a search

--- src/pages/UserDetalis/UserDetalis.jsx
+++ src/pages/UserDetalis/UserDetalis.jsx
@@ -17,39 +17,39 @@ const UserDetalis = () => {
 						<div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Surname</p>
-								<p className={styles.info}>{detalisUser.name}</p>
+								<p className={styles.info}>{detalisUser?.name}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Name</p>
-								<p className={styles.info}>{detalisUser.username}</p>
+								<p className={styles.info}>{detalisUser?.username}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Email</p>
-								<p className={styles.info}>{detalisUser.email}</p>
+								<p className={styles.info}>{detalisUser?.email}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>City</p>
-								<p className={styles.info}>{detalisUser.address.city}</p>
+								<p className={styles.info}>{detalisUser?.address.city}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Street</p>
-								<p className={styles.info}>{detalisUser.address.street}</p>
+								<p className={styles.info}>{detalisUser?.address.street}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>House number</p>
-								<p className={styles.info}>{detalisUser.address.suite}</p>
+								<p className={styles.info}>{detalisUser?.address.suite}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Phone number</p>
-								<p className={styles.info}>{detalisUser.phone}</p>
+								<p className={styles.info}>{detalisUser?.phone}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Company</p>
-								<p className={styles.info}>{detalisUser.company.name}</p>
+								<p className={styles.info}>{detalisUser?.company.name}</p>
 							</div>
 							<div className={styles.blockInfo}>
 								<p className={styles.title}>Website</p>
-								<p className={styles.info}>{detalisUser.website}</p>
+								<p className={styles.info}>{detalisUser?.website}</p>
 							</div>
 						</div>
 						<img
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,9 +1,19 @@
-import React, {useContext , useEffect } from "react";
+/* eslint-disable array-callback-return */
+import React, {useContext , useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Users.module.css";
 import { UserContext } from "../../context/context";
 const Users = () => {
    const { users, setUsers } = useContext(UserContext);
+   
+   const [user, setUser] = useState("");
+
+	const getProduct = (data) => {
+		const id = data.id;
+		let cart = JSON.parse(localStorage.getItem("cart")) || {};
+		cart[id] = { ...data, count: 1 };
+		localStorage.setItem("card", JSON.stringify(cart));
+	};
 
 	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/users")
@@ -19,50 +29,69 @@ const Users = () => {
 	}, [setUsers]);
 	return (
 		<div className={styles.container}>
-			<h1>user information</h1>
+			<div className={styles.Header}>
+				<h1>user information</h1>
+				<input
+					onChange={(e) => {
+						setUser(e.target.value);
+					}}
+					type="text"
+					placeholder="Search"
+				/>
+			</div>
+
 			<div className={styles.users}>
-				{users.map((item) => (
-					<div className={styles.userBlock} key={item.id}>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Surname</p>
-							<p className={styles.info}>{item.name}</p>
+				{users
+					.filter((val) => {
+						if (user === "") {
+							return val;
+						} else if (val.name.toLowerCase().includes(user.toLowerCase())) {
+							return val;
+						}
+					})
+					.map((item) => (
+						<div className={styles.userBlock} key={item.id}>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Surname</p>
+								<p className={styles.info}>{item.name}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Name</p>
+								<p className={styles.info}>{item.username}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Email</p>
+								<p className={styles.info}>{item.email}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>City</p>
+								<p className={styles.info}>{item.address.city}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Street</p>
+								<p className={styles.info}>{item.address.street}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>House number</p>
+								<p className={styles.info}>{item.address.suite}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Phone number</p>
+								<p className={styles.info}>{item.phone}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Company</p>
+								<p className={styles.info}>{item.company.name}</p>
+							</div>
+							<div className={styles.blockInfo}>
+								<p className={styles.title}>Website</p>
+								<p className={styles.info}>{item.website}</p>
+							</div>
+							<Link onClick={() => getProduct(item)} to={`/detalis/${item.id}`}>
+								More about the user
+							</Link>
 						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Name</p>
-							<p className={styles.info}>{item.username}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Email</p>
-							<p className={styles.info}>{item.email}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>City</p>
-							<p className={styles.info}>{item.address.city}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Street</p>
-							<p className={styles.info}>{item.address.street}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>House number</p>
-							<p className={styles.info}>{item.address.suite}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Phone number</p>
-							<p className={styles.info}>{item.phone}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Company</p>
-							<p className={styles.info}>{item.company.name}</p>
-						</div>
-						<div className={styles.blockInfo}>
-							<p className={styles.title}>Website</p>
-							<p className={styles.info}>{item.website}</p>
-                  </div>
-                  <Link to={`/detalis/${item.id}`} >More about the user</Link>
-               </div>
-            ))
-            }
+					))}
 			</div>
 		</div>
 	);
